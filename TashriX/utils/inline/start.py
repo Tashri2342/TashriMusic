b418646c284b9178796c0ca8b0c9106248e844fa
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-08-04 15:58:26 +0530

    start.py

--- TashriX/utils/inline/start.py
+++ TashriX/utils/inline/start.py
@@ -47,10 +47,5 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER
             )
         ],
-        [
-            InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url=config.UPSTREAM_REPO
-            )
-        ],
      ]
     return buttons
